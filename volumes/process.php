748c615e10f00a1f0dadc96188d997dc3286a357
wagner <wagncarv@gmail.com>
2022-02-02 02:36:30 -0300

    projeto php trabalhar com volumes

---
+++ volumes/process.php
@@ -0,0 +1,22 @@
+<?php
+
+    $message = $_POST["message"];
+
+    $files = scandir("./messages/");
+    $num_files = count($files);
+
+    if (is_bool($num_files)) {
+        $filename = "msg-0.txt";
+    } 
+    else {
+        $filename = "msg-{$num_files}.txt";
+    }
+    $file = fopen("./messages/{$filename}", "w");
+
+    fwrite($file, $message);
+    fclose($file);
+
+    echo("<p>Mensagem: {$message}\nArquivo: {$filename}</p>");
+    // header("Location: index.php");
+
+?>
