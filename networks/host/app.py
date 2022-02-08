9078748c1f10d997e641351617e59d6a7314ba67
wagner <wagncarv@gmail.com>
2022-02-08 19:19:09 -0300

    docker host conexao api externa

--- networks/host/app.py
+++ networks/host/app.py
@@ -7,10 +7,10 @@ from flask_mysqldb import MySQL
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config["MYSQL_HOST"] = "host.docker.internal"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "root"
-app.config["MYSQL_DB"] = "flaskhost"
+app.config['MYSQL_HOST'] = 'host.docker.internal'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'flaskhost'
 
 mysql = MySQL(app)
 
@@ -20,11 +20,11 @@ def index():
     return data.json()
 
 @app.route("/inserthost", methods=["POST"])
-def index():
-    data = requests('https://randomuser.me/api').json()
+def inserthost():
+    data = requests.get('https://randomuser.me/api').json()
     username = data['results'][0]['name']['first']
     cur = mysql.connection.cursor()
-    cur.execute("""INSERT INTO users(name) VALUES(%s)""", (username))
+    cur.execute("""INSERT INTO users(name) VALUES(%s)""", (username,))
     mysql.connection.commit()
     cur.close()
     return username
